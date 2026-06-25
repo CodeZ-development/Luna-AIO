07c5299888b85d12f71bb3d2efa4956efa91837a
CodeZ <tanmay567hjjjj@gmail.com>
2026-06-25 10:07:35 +0530

    Initial commit: Luna AIO

---
+++ src/core/emoji.js
@@ -0,0 +1,45 @@
+"use strict";
+module.exports = {
+    dot: "<a:dot:1517777535306563695>",
+    reply: "<:reply:1517778056683589652>",
+    arrow: "<a:arrow_arrow:1517778307951759471>",
+
+    online: "<:online:1517778552747986996>",
+    idle: "<a:idle:1517778752732663858>",
+    dnd: "<:dnd:1517778663737917641>",
+    offline: "<:offline:1517778588785705052>",
+
+    success: "<a:success:1517779559863292036>",
+    error: "<a:error:1517779657481654373>",
+    warning: "<:warning:1517782534119755869>",
+    loading: "<a:loding:1517780200341897246>",
+
+    ban: "<:BAN:1517780782490324993>",
+    kick: "<:kick:1517782399348375674>",
+    mute: "<a:muted:1517780890413957213>",
+    warn: "<a:WanringAlert:1517780187734081587>",
+    unban: "<:unban:1517780834277658634>",
+
+    shield: "<:Sheild:1517783023259619358>",
+    lock: "<:lock:1517782990929793144>",
+    unlock: "<:unlock:1517782985540239441>",
+    panic: "<a:Danger:1517788549275975781>",
+    whitelist: "<:INGAME_WHITELISTED:1517783255787638815>",
+    quarantine: "<:Qurantine:1517786235672723537>",
+
+    channel: "<a:channel:1517783641227526194>",
+    role: "<:roles:1517783638505291787>",
+    webhook: "<:Webhook:1517788225001885830>",
+    bot: "<a:Bots:1517783808504762438>",
+    owner: "<:owner:1517788162267680839>",
+    member: "<:Member:1517784052113870992>",
+
+    settings: "<a:settings:1517784207752040539>",
+    logs: "<:logs:1517784203318661174>",
+    stats: "<a:stats:1517784597721649352>",
+    link: "<:22273tgnewsemoji007:1517784465458597978>",
+    trash: "<:trash:1517784468793065574>",
+    ping: "<a:ping:1517784598543728691>",
+    server: "<a:server:1517784853326598225>",
+    invite: "<:invite:1517784724083441784>"
+};
